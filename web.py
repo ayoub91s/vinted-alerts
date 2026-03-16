dc103a9561c03c15f982cdbf40d4a1fc5c5bd853
ayoub91s <ayoubdhif91350@gmail.com>
2026-03-16 17:47:52 +0100

    fix port web

--- web.py
+++ web.py
@@ -70,5 +70,5 @@ def api_acheter(item_id):
         return jsonify({"succes": False, "message": str(e)})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
